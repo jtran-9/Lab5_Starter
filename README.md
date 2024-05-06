052abbf8511e2b23eb26cd58fc1d5ee770cbc0fc
Arjun Kohli <arjun.kohli0707@gmail.com>
2024-05-05 19:46:38 -0700

    tests added

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Justin Tran <br>
 Arjun Kohli <br>
 Anthony Chapov
+
+1) No, I would not use unit tests to test the message feature of a messaging application as I believe the message feature would involve too many moving parts where you're not only sending a message, but that message is supposed to be received by another user too. Those messages can't disappear either, they have to be saved forever to some sort of database. Since there are quite a few individual components that interact with each other, unit testing would probably not be the best option here.
+
+
+2) Yes, I would use unit tests to test the “max message length” feature of a messaging application as unit testing can ensure that a simple feature like checking the message length functions as intended and this won't be affected by the other features either.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,37 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+test('isPhoneNumber works correctly', () => {
+  expect(isPhoneNumber('123-456-6890')).toBe(true);
+  expect(isPhoneNumber('(123)456-6890')).toBe(true);
+  expect(isPhoneNumber('1234aa6890')).toBe(false);
+  expect(isPhoneNumber('123-45-6890')).toBe(false);
+});
+
+test('isEmail works correctly', () => {
+  expect(isEmail('a2kohli@ucsd.edu')).toBe(true);
+  expect(isEmail('a2kohli@gmail.com')).toBe(true);
+  expect(isEmail('a2kohli@.edu')).toBe(false);
+  expect(isEmail('a2kohli@ucsd')).toBe(false);
+});
+
+test('isStrongPassword works correctly', () => {
+  expect(isStrongPassword('a123')).toBe(true);
+  expect(isStrongPassword('mem_munchers123')).toBe(true);
+  expect(isStrongPassword('memory_munchers123')).toBe(false);
+  expect(isStrongPassword('a#123')).toBe(false);
+});
+
+test('isDate works correctly', () => {
+  expect(isDate('01/02/2023')).toBe(true);
+  expect(isDate('1/2/2023')).toBe(true);
+  expect(isDate('01-02-2023')).toBe(false);
+  expect(isDate('a-b-2023')).toBe(false);
+});
+
+test('isHexColor works correctly', () => {
+  expect(isHexColor('fff')).toBe(true);
+  expect(isHexColor('abcdef')).toBe(true);
+  expect(isHexColor('ghi')).toBe(false);
+  expect(isHexColor('1-2@45')).toBe(false);
+});
